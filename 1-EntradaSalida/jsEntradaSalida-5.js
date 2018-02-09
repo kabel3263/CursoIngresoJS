efe30cca40fe6f9836d05a9bf65bc1c8cd284d92
kabel3263 <36267433+kabel3263@users.noreply.github.com>
2018-02-09 10:47:37 -0300

    ejercicios del 1 al 6 en clases
    alert, prompt, concatenar, parseint

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,15 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+    //aca defino las variables
+    var nombre;
+    var edad
+    nombre=document.getElementById("elNombre").value;
+    edad=document.getElementById("laEdad").value;
+    //nombre pero para dejar espacio se debe colocar comillas con un espacio entre ellas asi como se muestra
+    //alert(nombre+" "+edad);
+    alert("Usted se llama "+nombre+" y tiene "+edad+" años");
+    //esta segunda parte seria la correcta
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,5 +4,33 @@ Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
+    //INGRESAR VARIABLES + INGRESAR DATOS + OPERAR + MOSTRAR RESULTADOS
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+
+    numeroUno=document.getElementById("numeroUno").value;
+    numeroDos=document.getElementById("numeroDos").value;
+
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    resultado=numeroUno+numeroDos;
+    alert(resultado)
+    
+
+    //el alert puede ser tambien asi: alert(numeroUno+numeroDos) y quito el resultado de la variable y de la parte de abajo 
+    /*var numeroUno;
+    var numeroDos;
+    
+
+    numeroUno=document.getElementById("numeroUno").value;
+    numeroDos=document.getElementById("numeroDos").value;
+
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    alert(numeroUno+numeroDos);*/
 }
+
 
