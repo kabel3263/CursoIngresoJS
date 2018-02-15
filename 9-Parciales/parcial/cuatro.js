f08608a8e8901dd510a33ffb6a8853771ce1b73e
kabel3263 <36267433+kabel3263@users.noreply.github.com>
2018-02-15 11:26:18 -0300

    Add files via upload
    if, else, document.write, prompt
    
    ==
    ===
    !=
    >
    <
    >=
    <=

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,41 @@
 function Mostrar()
 {
+var num1;
+var num2;
+var multiplicacion;
+var resta;
+var suma;
 
+num1= prompt("ingrese num1"+" ");
+num2= prompt("ingrese num2"+" ");
+
+num1= parseInt (num1);
+num2= parseInt (num2);
+
+if(num1===num2){
+
+    multiplicacion= num1*num2;
+    document.write("La multiplicacion es:"+" "+multiplicacion);
+} else {
+
+    if (num1>num2){
+        resta= num1-num2;
+        document.write("La resta es:"+" "+resta);
+    } else {
+
+        suma=num1+num2;
+        document.write("la suma es="+" "+suma);
+        
+    }
+
+}
+
+
+}
+/* if (num1===num2){
+    resultado=num1*num2;
+} else if(num1>num2){
+    res=num1-num2;
+} else {
+    res=num1+num2;
 }
