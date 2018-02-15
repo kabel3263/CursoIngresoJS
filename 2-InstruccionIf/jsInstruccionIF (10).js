bbee523dfb13973b9a370a3eed3e7ae8d72528a5
kabel3263 <36267433+kabel3263@users.noreply.github.com>
2018-02-15 12:12:54 -0300

    Add files via upload
    math.random()

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,19 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var nota;
+	nota=parseInt(math.random()*(10-1)+1);
+
+	if(nota>=9){
+		alert("EXCELENTE");
+	}else if (nota>=6&&nota<=8){
+		alert("MUY BIEN");
+	}else if (nota>=4&&nota<=5){
+		alert("BIEN");
+	}else if (nota>=3&&nota<=2){
+		alert("MAL");
+	}else {
+		alert("Desastrozo");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,15 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var num1; 
+	num1= parseInt((Math.random()*(11-1))+1);
+
+	alert("Tu numero:" +" "+ num1);
+
+    
 
 }//FIN DE LA FUNCIÓN
+
+
+/*
+(math.random()*(max-min)+min)  */
