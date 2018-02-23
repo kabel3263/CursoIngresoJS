602413a886ab9b571858c6a4161f8c6070fb19f2
kabel3263 <36267433+kabel3263@users.noreply.github.com>
2018-02-23 11:57:44 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,16 +2,55 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var negativos=0;
+	var acumuPosit=0;
+	var acumuNegat=0;
+	var positivos=0;
+	var contadorCeros;
+	var promPositivos;
+	var promNegativos;
+	var diferencia;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
 	while(respuesta!="no")
 	{
+		contador++;
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+	
+
+		if(numero%2==0)
+		{
+			acumNegat=acumuNegat+numero;
+			negativos=negativos+numero;
+			negativos++;
+			
+		}else {
+			if(numero>0){
+				acumuPosit=acumuPosit+numero;
+				positivos ++;
+	
+			}else{
+				contadorCeros++;
+			}
+		}
+		repuesta=prompt("ingrese no para salir");
 		
 	
-	}
+	}//while(repuesta!="no")
 
+	promPositivos=acumuPosit/negativos;
+	promNegativos=acumuNegat/positivos;
+	diferencia=positivos-negativos;
+
+
+	document.write("los negativos son"+" "+negativos);
+	document.write("los positivos son"+" "+positivos);
+	document.write("promPositivos es"+" "+promPositivos);
+	document.write("promNegativo es"+" "+promNegativo);
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,16 +3,37 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var numero;
+	var maximo;
+	var minimo;
 	var respuesta='si';
-
+	
 	while(respuesta!='no')
 	{
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+
+		if(contador==0){
+			maximo=numero;
+			minimo=numero;
+		}else
 		
-	
+			if(numero>maximo){
+
+				maximo=numero;
+			}
+			if(numero<minimo){
+
+			minimo=numero
+		}
+
 	}
+	contador=contador+1;
+	repuesta=prompt("para salir , ingresar no");
+}
+
+document.getElementById("maximo").value=maximo;
+document.getElementById("minimo").value=minimo;
 
 
-
-
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
